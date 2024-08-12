cf086cdca16d2ed9718cd788a7b32eac2d658fe5
phaedrusflow <70476764+PhaedrusFlow@users.noreply.github.com>
2024-08-12 11:31:21 -0700

    cross optimized

--- Linux/Cross/cross.sh
+++ Linux/Cross/cross.sh
@@ -16,47 +16,53 @@ if $IS_MACOS; then
     MACOS_ARGS="-DCMAKE_OSX_ARCHITECTURES=x86_64;arm64 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15"
 fi
 
-# LLVM
-mkdir -p llvm/build-debug
-cd llvm/build-debug
-cmake .. \
-  -DCMAKE_INSTALL_PREFIX=$HOME/local/llvm18-debug \
-  -DCMAKE_PREFIX_PATH=$HOME/local/llvm18-debug \
-  -DCMAKE_BUILD_TYPE=Debug \
-  -DLLVM_ENABLE_LIBXML2=OFF \
-  -DLLVM_ENABLE_TERMINFO=OFF \
-  -DLLVM_ENABLE_LIBEDIT=OFF \
-  -DLLVM_PARALLEL_LINK_JOBS=1 \
-  -G Ninja \
-  $MACOS_ARGS
-ninja install
-cd ../..
+# Get the current directory
+CURRENT_DIR=$(pwd)
 
-# LLD
-mkdir -p lld/build-debug
-cd lld/build-debug
-cmake .. \
-  -DCMAKE_INSTALL_PREFIX=$HOME/local/llvm18-debug \
-  -DCMAKE_PREFIX_PATH=$HOME/local/llvm18-debug \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DLLVM_PARALLEL_LINK_JOBS=1 \
-  -DCMAKE_CXX_STANDARD=17 \
-  -G Ninja \
-  $MACOS_ARGS
-ninja install
-cd ../..
+# Common arguments for all builds
+COMMON_ARGS=(
+    -DCMAKE_INSTALL_PREFIX=$CURRENT_DIR/install
+    -DCMAKE_PREFIX_PATH=$CURRENT_DIR/install
+    -DCMAKE_BUILD_TYPE=Release
+    -DLLVM_ENABLE_PROJECTS="clang;lld;mlir;polly;bolt;clang-tools-extra;compiler-rt;cross-project-tests;libc;libclc;lldb;openmp;pstl;flang"
+    -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind;compiler-rt;openmp"
+    -DLLVM_TARGETS_TO_BUILD="X86;AArch64;NVPTX;ARM;AMDGPU;PowerPC"
+    -DLLVM_ENABLE_ASSERTIONS=ON
+    -DLLVM_ENABLE_DOXYGEN=ON
+    -DLLVM_BUILD_DOCS=ON
+    -DLLVM_ENABLE_SPHINX=ON
+    -DLLVM_ENABLE_LIBXML2=ON
+    -DLLVM_ENABLE_TERMINFO=ON
+    -DLLVM_ENABLE_LIBEDIT=ON
+    -DLLVM_PARALLEL_LINK_JOBS=1
+    -DLLVM_ENABLE_ZLIB=ON
+    -DLLVM_ENABLE_THREADS=ON
+    -DLLVM_ENABLE_LTO=ON
+    -DLLVM_ENABLE_RTTI=OFF
+    -DLLVM_ENABLE_EH=OFF
+    -DLLVM_USE_INTEL_JITEVENTS=ON
+    -DLLVM_ENABLE_Z3_SOLVER=ON
+    -DCMAKE_CXX_STANDARD=17
+    -DLLVM_INCLUDE_TESTS=OFF
+    -G Ninja
+    $MACOS_ARGS
+)
 
-# Clang
-mkdir -p clang/build-debug
-cd clang/build-debug
-cmake .. \
-  -DCMAKE_INSTALL_PREFIX=$HOME/local/llvm18-debug \
-  -DCMAKE_PREFIX_PATH=$HOME/local/llvm18-debug \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DLLVM_PARALLEL_LINK_JOBS=1 \
-  -DLLVM_INCLUDE_TESTS=OFF \
-  -G Ninja \
-  $MACOS_ARGS
+# Create build directory in the current directory
+mkdir -p build
+cd build
+
+# Configure LLVM, Clang, and LLD build
+cmake ../llvm "${COMMON_ARGS[@]}"
+
+# Build and install LLVM, Clang, and LLD
 ninja install
-cd ../..
+
+# Generate Doxygen documentation
+ninja doxygen-llvm doxygen-clang doxygen-lld
+
+# Return to the original directory
+cd ..
+
+echo "LLVM, Clang, and LLD build complete. Installed to $CURRENT_DIR/install"
 
